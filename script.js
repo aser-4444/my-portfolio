7e63be0bff6719f0ba01975ffac47a26888cd532
Aser mosbah <aserica.asermosbah@gmail.com>
2026-08-31 12:44:51 +0200

    add hamburger menu for mobile navbar

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const btn = document.getElementById("darkModeBtn");
 btn.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
 });
+
+const humburger = document.getElementById("hamburgerBtn");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", function() {
+    navLinks.classList.toggle("active");
+});
