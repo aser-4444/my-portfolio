91fbeb4264f88cbe5ff486ae9549e5ab88545ac8
Aser mosbah <aserica.asermosbah@gmail.com>
2026-09-19 00:04:43 +0200

    scrollop button to the page

--- script.js
+++ script.js
@@ -28,4 +28,18 @@ window.addEventListener("scroll", function() {
     } else {
         navbar.classList.remove("scrolled");
     }
-});    
+});
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 300) {
+        scrollTopBtn.style.display = "block";
+    } else {
+        scrollTopBtn.style.display = "none";
+    }
+});
+
+scrollTopBtn.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
